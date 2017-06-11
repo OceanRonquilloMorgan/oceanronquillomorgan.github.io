e76f26d71c164e6c43c81ef1a5187f280bb33333
Ocean Ronquillo-Morgan <oronquil@usc.edu>
2017-06-11 02:49:56 -1000

    easter egg

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 $(document).ready(function(){
 	$("#fighton").click(function() {
     	$('#hawaii.jumbotron.text-center').toggleClass("newbg");
+    	$('#waves.jumbotron.text-center').toggleClass("newbg");
+    	$('#code.jumbotron.text-center').toggleClass("newbg");
+    	$('#sunset.jumbotron.text-center').toggleClass("newbg");
   	});
 })
