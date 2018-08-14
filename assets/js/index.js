81f766778ad16015eb75c0ee68c03e0cc222e2d1
Ocean Ronquillo-Morgan <oronquil@usc.edu>
2018-08-14 09:45:27 +0200

    Added About, Skills, and Message Sections & more front-end aesthetics :art:

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,6 +2,22 @@
 AOS.init();
 
 // CounterUp.js - jQuery plugin for number animation
-$('.counter').counterUp();
+$('.counter').counterUp({
+  delay: 10,
+  time: 400
+});
+
+// ScrollTo.js - smooth scrolling jQuery plugin
+$('#scroll').click(function() {
+  $([document.documentElement, document.body]).animate(
+    {
+      scrollTop: $('#about').offset().top
+    },
+    400
+  );
+});
+
+// Bulma Carousels
+var carousels = bulmaCarousel.attach(); // carousels now contains an array of all Carousel instances
 
 // TO-DO: Implement modal toggle
