fe18a0a1a2cb16af588a38e6aa1d1862f3b3ca80
Ocean Ronquillo-Morgan <oronquil@usc.edu>
2018-08-16 19:33:33 -0400

    Finallized all sections! Read for the overhaul to be live :ok_hand:

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,20 @@
 // AOS.js - animation on scroll
 AOS.init();
 
+/* Implements Bulma modal pop-up using jQuery
+ * Requires: toggle the is-active class on the modal div,
+             toggle the is-clipped class on the <html> element
+ */
+function toggleModalClasses(event) {
+  var modalId = event.currentTarget.dataset.modalId;
+  var modal = $(modalId);
+  modal.toggleClass('is-active');
+  $('html').toggleClass('is-clipped');
+}
+
+$('.open-modal').click(toggleModalClasses);
+$('.close-modal').click(toggleModalClasses);
+
 // CounterUp.js - jQuery plugin for number animation
 $('.counter').counterUp({
   delay: 10,
@@ -16,8 +30,3 @@ $('#scroll').click(function() {
     400
   );
 });
-
-// Bulma Carousels
-var carousels = bulmaCarousel.attach(); // carousels now contains an array of all Carousel instances
-
-// TO-DO: Implement modal toggle
